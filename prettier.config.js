425527c649f3810d45241780c9e450e124c6e17d
nakyeonko3 <badaclock@gmail.com>
2024-10-30 00:21:09 +0900

    settings: eslint, prettier, lefthook 설정 (#1)

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+export default {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+};

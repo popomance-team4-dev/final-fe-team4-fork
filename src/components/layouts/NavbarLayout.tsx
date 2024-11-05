e663c77e13723048239e80d9eceb3239d2c6b334
nakyeonko3 <badaclock@gmail.com>
2024-11-05 15:45:10 +0900

    [fix] tailwind 재설정, pretendard폰트 적용 #14
    - tailwindcss가 적용되지 않는 문제 수정
    - tailwind.config.js 파일이 수정
    - content 부분에 ts, tsx가 추가

--- src/components/layouts/NavbarLayout.tsx
+++ src/components/layouts/NavbarLayout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
 
-import Navbar from './Navbar';
+import { NavSidebar } from '../NavSidebar';
 
 const NavbarLayout = () => {
   return (
     <>
       <div className="flex min-h-screen bg-gray-50">
-        <Navbar />
-        <main className="flex-1 ml-[240px] p-6">
+        <NavSidebar />
+        <main className="flex-1 p-3">
           <Outlet />
         </main>
       </div>

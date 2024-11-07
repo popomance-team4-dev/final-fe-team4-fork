94008f7fddc4489b36b898baa10c1da15460236c
dhkim <ddhkim11@gmail.com>
2024-11-08 04:39:19 +0900

    [settings] 전체 lodash 패키지 대신 throttle 함수만 사용할 수 있게 변경 #43

--- src/components/ui/AudioPlayer.tsx
+++ src/components/ui/AudioPlayer.tsx
@@ -1,6 +1,4 @@
-'use client';
-
-import { throttle } from 'lodash';
+import throttle from 'lodash.throttle';
 import * as React from 'react';
 import WaveSurfer, { WaveSurferOptions } from 'wavesurfer.js';
 

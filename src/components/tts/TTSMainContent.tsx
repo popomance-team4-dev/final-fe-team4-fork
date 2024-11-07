2138d75be09d08fbba7ab12d97e9219f20762224
Gin Song <sjgaru@gmail.com>
2024-11-08 00:48:18 +0900

    [setting] 깃헙액션 정리 #61

--- src/components/tts/TTSMainContent.tsx
+++ src/components/tts/TTSMainContent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Button } from '@/components/ui/commonbutton';
-import { SaveButton, UploadButton } from '@/components/ui/iconButton';
+import { Button } from '@/components/ui/CommonButton';
+import { SaveButton, UploadButton } from '@/components/ui/IconButton';
 
 const TTSMainContent: React.FC = () => {
   return (

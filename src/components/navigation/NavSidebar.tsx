db980216201efcd3296e3c57ed5eb79edce1f244
nakyeonko3 <badaclock@gmail.com>
2024-11-15 02:19:17 +0900

    [feact] 메뉴바 확장 버튼 추가 및 확장 기능 #91

--- src/components/navigation/NavSidebar.tsx
+++ src/components/navigation/NavSidebar.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { TbFolders, TbFolderShare, TbSmartHome } from 'react-icons/tb';
+import { FC, useState } from 'react';
+import { TbFolders, TbFolderShare, TbLayoutSidebar, TbSmartHome } from 'react-icons/tb';
 
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
@@ -12,17 +12,17 @@ interface SidebarButtonProps {
 
 const SidebarButton: FC<SidebarButtonProps> = ({ icon: Icon, label }) => {
   return (
-    <div className="flex w-full  flex-col  items-center px-2 py-2 ml-[-20px] rounded-lg cursor-pointer hover:bg-gray-50 group-hover/navbar:py-2  group-hover/navbar:flex-row  group-hover/navbar:ml-2">
-      <Icon className="min-w-[24px] h-6 text-[#1b1b1b]" />
+    <div className="flex w-full  flex-col  items-center px-2 py-2 ml-[-20px] rounded-lg cursor-pointer hover:bg-gray-50 group-[.expanded]/navbar:py-2  group-[.expanded]/navbar:flex-row  group-[.expanded]/navbar:ml-2">
+      <Icon className="min-w-6 h-6 text-[#1b1b1b]" />
       <span
         className="ml-2.5 text-black overflow-hidden whitespace-nowrap   
-        w-0 h-0 opacity-0 group-hover/navbar:w-auto group-hover/navbar:opacity-100  group-hover/navbar:h-auto"
+        w-0 h-0 opacity-0 group-[.expanded]/navbar:w-auto group-[.expanded]/navbar:opacity-100  group-[.expanded]/navbar:h-auto"
       >
         {label}
       </span>
       <div
         className={
-          'group-hover/navbar:absolute  text-[11px] opacity-100 group-hover/navbar:opacity-0'
+          'group-[.expanded]/navbar:absolute  text-[11px] opacity-100 group-[.expanded]/navbar:opacity-0'
         }
       >
         {label}
@@ -32,12 +32,16 @@ const SidebarButton: FC<SidebarButtonProps> = ({ icon: Icon, label }) => {
 };
 
 export function NavSidebar() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-    <div className="flex h-screen w-[104px] hover:w-[244px] flex-col border-r bg-white p-4 group/navbar">
+    <div
+      className={`flex h-screen w-[104px] flex-col border-r bg-white p-4 group/navbar ${isExpanded ? `expanded w-[244px]` : ''}`}
+    >
       <div className="my-2">
         <div className="flex items-center gap-2 font-bold text-xl ">
           <span className="text-emerald-600">AI</span>
-          <span className="opacity-0 group-hover/navbar:opacity-100">PARK</span>
+          <span className="opacity-0 group-[.expanded]/navbar:opacity-100 ">PARK</span>
         </div>
       </div>
 
@@ -48,19 +52,27 @@ export function NavSidebar() {
       </Button>
 
       <ScrollArea className="scrollArea py-6">
-        <div className="flex flex-col items-center w-full space-y-1 text-black text-body2 ml-2 group-hover/navbar:items-start">
+        <div className="flex flex-col items-center w-full space-y-1 text-black text-body2 ml-2 group-[.expanded]/navbar:items-start">
           <div className="mr-4">
-            <h2 className="py-2 text-overline self-start">General</h2>
+            <h2 className="py-2 text-overline self-start group-[.expanded]/navbar:ml-2">General</h2>
           </div>
           <SidebarButton icon={TbSmartHome} label="홈" />
           <SidebarButton icon={TbFolders} label="프로젝트" />
           <SidebarButton icon={TbFolderShare} label="내보내기" />
         </div>
         <Separator className="my-4 " />
-        <div className="py-2 ml-1">
-          <h2 className="text-overline text-black whitespace-nowrap">Workspace</h2>
+        <div className="py-2 ml-1 ">
+          <h2 className="text-overline text-black whitespace-nowrap group-[.expanded]/navbar:ml-1">
+            Workspace
+          </h2>
         </div>
       </ScrollArea>
+      <div className="mt-auto h-20">
+        <Separator className="my-4 " />
+        <button className="mt-2 ml-4" onClick={() => setIsExpanded(!isExpanded)}>
+          <TbLayoutSidebar className="w-6 h-6 group-[.expanded]/navbar:text-gray-300 text-black " />
+        </button>
+      </div>
     </div>
   );
 }

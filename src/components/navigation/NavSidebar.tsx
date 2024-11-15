21069dc9e88452c1b3c84c7d2355984cffef0bfb
nakyeonko3 <badaclock@gmail.com>
2024-11-15 11:26:44 +0900

    [fix] 네비바 프로젝트 탭 아이콘 추가  #91

--- src/components/navigation/NavSidebar.tsx
+++ src/components/navigation/NavSidebar.tsx
@@ -1,8 +1,15 @@
 import { FC, useState } from 'react';
-import { TbFolders, TbFolderShare, TbLayoutSidebar, TbSmartHome } from 'react-icons/tb';
+import {
+  TbFileDatabase,
+  TbFileMusic,
+  TbFileTypography,
+  TbFolders,
+  TbFolderShare,
+  TbLayoutSidebar,
+  TbSmartHome,
+} from 'react-icons/tb';
 
 import { Button } from '@/components/ui/button';
-import { ScrollArea } from '@/components/ui/scroll-area';
 import { Separator } from '@/components/ui/separator';
 
 interface SidebarButtonProps {
@@ -12,8 +19,8 @@ interface SidebarButtonProps {
 
 const SidebarButton: FC<SidebarButtonProps> = ({ icon: Icon, label }) => {
   return (
-    <div className="flex w-full  flex-col  items-center px-2 py-2 ml-[-20px] rounded-lg cursor-pointer hover:bg-gray-50 group-[.expanded]/navbar:py-2  group-[.expanded]/navbar:flex-row  group-[.expanded]/navbar:ml-2">
-      <Icon className="min-w-6 h-6 text-[#1b1b1b]" />
+    <div className="flex w-full  flex-col h-[52px] items-center px-2 ml-[-20px] rounded-lg cursor-pointer hover:text hover:bg-gray-50 group-[.expanded]/navbar:py-2  group-[.expanded]/navbar:flex-row  group-[.expanded]/navbar:ml-2">
+      <Icon className="min-w-6 h-6 text-black" />
       <span
         className="ml-2.5 text-black overflow-hidden whitespace-nowrap   
         w-0 h-0 opacity-0 group-[.expanded]/navbar:w-auto group-[.expanded]/navbar:opacity-100  group-[.expanded]/navbar:h-auto"
@@ -51,7 +58,7 @@ export function NavSidebar() {
         새 프로젝트 생성
       </Button>
 
-      <ScrollArea className="scrollArea py-6">
+      <div className="scrollArea py-6">
         <div className="flex flex-col items-center w-full space-y-1 text-black text-body2 ml-2 group-[.expanded]/navbar:items-start">
           <div className="mr-4">
             <h2 className="py-2 text-overline self-start group-[.expanded]/navbar:ml-2">General</h2>
@@ -66,7 +73,12 @@ export function NavSidebar() {
             Workspace
           </h2>
         </div>
-      </ScrollArea>
+        <div className="translate-x-4 translate-y-2 group-[.expanded]/navbar:ml-3">
+          <SidebarButton icon={TbFileTypography} label="TTS" />
+          <SidebarButton icon={TbFileMusic} label="VC" />
+          <SidebarButton icon={TbFileDatabase} label="CONCAT" />
+        </div>
+      </div>
       <div className="mt-auto h-20">
         <Separator className="my-4 " />
         <button className="mt-2 ml-4" onClick={() => setIsExpanded(!isExpanded)}>

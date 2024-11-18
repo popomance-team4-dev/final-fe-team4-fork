1699c5608c807adc1ee42232c93c3bb5be51a920
nakyeonko3 <badaclock@gmail.com>
2024-11-19 03:36:33 +0900

    [setting] env 변수에 대한 유효성 검증 로직 추가  #122
    - Update src/api/axios-client.ts

--- src/api/axios-client.ts
+++ src/api/axios-client.ts
@@ -2,6 +2,10 @@ import axios, { AxiosRequestConfig } from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
+if (!BASE_URL) {
+  throw new Error('VITE_API_URL is not defined');
+}
+
 export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> => {
   const instance = axios.create({
     baseURL: BASE_URL,

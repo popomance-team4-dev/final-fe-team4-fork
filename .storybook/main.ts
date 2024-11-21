5427863bac48bf739653f99ad7fba2977304fed9
dhkim <ddhkim11@gmail.com>
2024-11-21 22:52:33 +0900

    [settings] 스토리북 설정 변경 #146

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,14 +4,7 @@ import path from 'path';
 const config: StorybookConfig = {
   stories: ['../src/stories/**/*.stories.@(js|jsx|ts|tsx)'],
 
-  addons: [
-    '@storybook/addon-onboarding',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-links',
-    '@storybook/addon-themes',
-    '@chromatic-com/storybook',
-  ],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-themes', '@chromatic-com/storybook'],
 
   framework: {
     name: '@storybook/react-vite',

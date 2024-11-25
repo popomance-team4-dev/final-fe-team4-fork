25f047a5e452ace83a967afa8e12eaa1c54bbe5b
dhkim <ddhkim11@gmail.com>
2024-11-25 23:56:51 +0900

    ..

--- src/hooks/useTextFileUpload.ts
+++ src/hooks/useTextFileUpload.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { validateTextFile } from '@/utils/fileValidation';
+import { validateTextFile } from '@/utils/fileValidator';
 import { parseText } from '@/utils/textParser';
 
 interface UseTextFileUploadProps {

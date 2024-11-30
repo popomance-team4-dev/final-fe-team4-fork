6b7ff6b9bf9faf25653873fb53364e66cc1a8536
dhkim <ddhkim11@gmail.com>
2024-12-01 04:41:34 +0900

    [fix] authApi 에러 핸들링 개선

--- src/api/authAPI.ts
+++ src/api/authAPI.ts
@@ -1,3 +1,5 @@
+import { AxiosError } from 'axios';
+
 import { customInstance } from './axios-client';
 
 export const login = async (email: string, pwd: string) => {
@@ -17,11 +19,12 @@ export const login = async (email: string, pwd: string) => {
     // 세션 기반 인증에서는 별도로 토큰을 저장하지 않음.
     // 서버가 세션을 설정하면 브라우저가 쿠키를 통해 자동으로 세션을 관리합니다.
   } catch (error) {
-    console.error('로그인 요청 실패:', error);
-
-    // 오류 메시지 처리
-    const errorMessage =
-      error.response?.data?.message || '로그인 요청에 실패했습니다. 다시 시도해주세요.';
-    throw new Error(errorMessage);
+    if (error instanceof AxiosError) {
+      // 오류 메시지 처리
+      const errorMessage =
+        error.response?.data?.message || '로그인 요청에 실패했습니다. 다시 시도해주세요.';
+      throw new Error(errorMessage);
+    }
+    throw error;
   }
 };

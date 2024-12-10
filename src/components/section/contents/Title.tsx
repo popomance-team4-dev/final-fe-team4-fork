1c22cd602ca082e0e2ceffc20a49dd73831d2640
dhkim <ddhkim11@gmail.com>
2024-12-10 22:33:07 +0900

    ..

--- src/components/section/contents/Title.tsx
+++ src/components/section/contents/Title.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
-import { TbCheck, TbPencil, TbX } from 'react-icons/tb';
+import { TbCheck, TbPencil } from 'react-icons/tb';
 import { Link } from 'react-router-dom';
 
-import { SaveButton } from '@/components/custom/buttons/IconButton';
+import { CloseButton, SaveButton } from '@/components/custom/buttons/IconButton';
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -39,7 +39,6 @@ const Title = ({
   const [editTitle, setEditTitle] = useState(projectTitle);
   const handleSaveVC = useVCStore((state) => state.handleSave);
 
-  // projectTitle prop이 변경될 때마다 editTitle 업데이트
   useEffect(() => {
     setEditTitle(projectTitle);
   }, [projectTitle]);
@@ -127,13 +126,7 @@ const Title = ({
         </div>
         <div className="flex items-center gap-2 -mt-1">
           <SaveButton onClick={handleSaveClick} />
-          <button
-            onClick={onClose}
-            className="flex items-center justify-center px-2.5 py-2 rounded-md border border-gray-200 text-gray-900 hover:bg-gray-50"
-          >
-            <TbX className="w-5 h-5" />
-            <span className="text-sm font-medium ml-1">닫기</span>
-          </button>
+          <CloseButton onClick={onClose} />
         </div>
       </div>
     </div>
